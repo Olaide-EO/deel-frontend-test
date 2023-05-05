1bc79887837695afbd0397140e5918ec6dfa66c5
Ebenezer Oladipo <ebenezerie@yahoo.com>
2023-05-05 20:43:29 +0100

    added comments to hooks and utils

--- src/utils/commonfunctions/index.ts
+++ src/utils/commonfunctions/index.ts
@@ -1,3 +1,16 @@
+/**
+ * This is a TypeScript function that returns a debounced version of a given function with a specified
+ * delay.
+ * @param {F} func - The function that needs to be debounced.
+ * @param {number} delay - The `delay` parameter is a number that represents the time in milliseconds
+ * for which the execution of the `func` function should be delayed after the last call to the returned
+ * function. This is used to implement a debounce function that delays the execution of a function
+ * until a certain amount of time has passed without
+ * @returns A debounced version of the input function `func`. The returned function takes the same
+ * arguments as `func` and will only execute `func` after a delay of `delay` milliseconds has passed
+ * since the last time the returned function was called. If the returned function is called again
+ * before the delay has passed, the previous timeout is cleared and a new one is set.
+ */
 export const debounce = <F extends (...args: any[]) => void>(
     func: F,
     delay: number,
--- src/utils/hooks/useOutsideClick.ts
+++ src/utils/hooks/useOutsideClick.ts
@@ -1,5 +1,15 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * This is a TypeScript function that takes a callback function as input and returns a reference to a
+ * div element, which triggers the callback when a click event occurs outside of the div element.
+ * @param callback - The `callback` parameter is a function that will be called when a click or touch
+ * event occurs outside of the element referenced by the `ref` object.
+ * @returns The `useOutsideClick` hook returns a `ref` object that can be attached to a DOM element.
+ * This `ref` object is created using the `useRef` hook and is of type `HTMLDivElement`. The `ref`
+ * object is used to detect clicks outside of the element it is attached to, and when a click is
+ * detected, the `callback` function passed to the hook is
+ */
 const useOutsideClick = (callback: () => void) => {
   const ref = useRef<HTMLDivElement>(null);
 
